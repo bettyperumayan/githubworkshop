21715995330b65a1f17fb4fde8caafde8d4a550e
BETTY PERUMAYAN <47517@fisat>
2019-05-15 04:59:29 -0400

    create a fun to add 3 and 4 num

--- git.py
+++ git.py
@@ -2,3 +2,11 @@ def add2(num1, num2):
   s = num1+num2
   return s
 print add2(1, 3)
+def add4(num1, num2, num3, num4):
+    s=num1+num2+num3+num4
+    return s
+print add4(1, 2, 3, 4)
+def add3(num1, num2, num3):
+    s=num1+num2+num3
+    return s
+print add3(1,23,4)
